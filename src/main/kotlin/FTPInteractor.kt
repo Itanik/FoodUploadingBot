12ad11376cace1d89965fd26abc00bc76aeddebd
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-05-13 01:32:04 +0300

    Add main functionality.

---
+++ src/main/kotlin/FTPInteractor.kt
@@ -0,0 +1,117 @@
+import com.github.kotlintelegrambot.Bot
+import com.github.kotlintelegrambot.entities.files.Document
+import ftp.FTPManager
+import ftp.FTPManager.Companion.foodImageFileName
+import ftp.FTPManager.Companion.foodJsonFileName
+import ftp.FTPManager.Companion.foodPath
+import ftp.FTPManager.Companion.menuJsonFileName
+import ftp.data.Credentials
+import ftp.data.Food
+import ftp.data.Menu
+import kotlinx.coroutines.Dispatchers.Default
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import org.apache.commons.net.ftp.FTPReply
+import java.io.File
+
+class FTPInteractor {
+    private val scope = MainScope()
+    private val credentials = getCredentials()
+    private val ftp by lazy { FTPManager(credentials) }
+
+    private fun getCredentials(): Credentials {
+        val credJson = FileManager.credentialsFile.bufferedReader().use {
+            it.readText()
+        }
+        return Json.decodeFromString(credJson)
+    }
+
+    fun downloadFile(bot: Bot, document: Document, onLoad: (File?) -> Unit) = scope.launch(Default) {
+        val bytes = bot.downloadFileBytes(document.fileId)
+        if (bytes == null) {
+            withContext(Main) { onLoad(null) }
+            return@launch
+        }
+
+        val file = FileManager.createFile(document.fileName!!).apply { writeBytes(bytes) }
+        withContext(Main) { onLoad(file) }
+    }
+
+    fun upload(menuFile: File?, tableFile: File?) {
+
+        try {
+            val connectionResult = ftp.connect()
+            if (!FTPReply.isPositiveCompletion(connectionResult)) throw Exception("Server refuse connection")
+
+            if (menuFile != null) {
+                val path = uploadMenuFile(menuFile)
+                uploadMenuJson(path)
+            }
+            if (tableFile != null) {
+                uploadTable(tableFile)
+                uploadUpdatedFoodFilesJson()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            ftp.disconnect()
+        }
+
+    }
+
+    private fun uploadUpdatedFoodFilesJson() {
+        val foodFiles = ftp.getTableFilesList()
+        println("Total number of food files: ${foodFiles.size}")
+        val foodData = foodFiles.map {
+            Food(it, foodPath.plus(it))
+        }
+        val format = Json { prettyPrint = true }
+        val json = format.encodeToString(foodData)
+        json.byteInputStream().use { inStream ->
+            if (ftp.uploadFile(foodPath.plus(foodJsonFileName), inStream)) {
+                println("Successfully uploaded $foodJsonFileName!")
+            } else
+                throw Exception("Failure! $foodJsonFileName does not uploaded")
+        }
+    }
+
+    private fun uploadTable(tableFile: File) {
+        tableFile.inputStream().use { inStream ->
+            if (ftp.uploadFile(foodPath.plus(tableFile.name), inStream))
+                println("Successfully uploaded ${tableFile.name}!")
+            else
+                throw Exception("Failure! ${tableFile.name} does not uploaded")
+        }
+    }
+
+    private fun uploadMenuJson(path: String) {
+        val format = Json { prettyPrint = true }
+        val json = format.encodeToString(Menu(path.plus("?").plus(System.currentTimeMillis())))
+        json.byteInputStream().use { inStream ->
+            if (ftp.uploadFile(foodPath.plus(menuJsonFileName), inStream)) {
+                println("Successfully uploaded $menuJsonFileName!")
+            } else
+                throw Exception("Failure! $menuJsonFileName does not uploaded")
+        }
+    }
+
+    private fun uploadMenuFile(menuFile: File): String {
+        val fileName = menuFile.name
+        val newName = foodImageFileName.plus(fileName.extension)
+        val newPath = foodPath.plus(newName)
+        menuFile.inputStream().use { inStream ->
+            if (ftp.uploadFile(newPath, inStream))
+                println("Successfully uploaded $newName!")
+            else
+                println("Failure! $newName does not uploaded")
+        }
+        return newPath
+    }
+
+    private val String.extension: String get() = this.split('.').last()
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,118 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.*
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.entities.InlineKeyboardMarkup
+import com.github.kotlintelegrambot.entities.ParseMode.MARKDOWN
+import com.github.kotlintelegrambot.entities.keyboard.InlineKeyboardButton
+import com.github.kotlintelegrambot.logging.LogLevel
+
+private lateinit var allowedUsers: List<String>
+
+fun main(appArgs: Array<String>) {
+    val botToken = appArgs.firstOrNull() ?: throw Exception("You should provide telegram token in app args")
+    allowedUsers = appArgs.filterNot { it == botToken }
+    if (allowedUsers.isEmpty()) throw Exception("You should provide nicknames of users, what will be able to use this bot")
+    if (!FileManager.credentialsFile.exists()) throw Exception("You should place credentials.json file in resources folder")
+
+    val interactor = FTPInteractor()
+
+    var menuProcessing = false
+    var tableProcessing = false
+    var lastProcessedImage = "none"
+    var lastProcessedMenuDocument = "none"
+    var lastProcessedTableDocument = "none"
+
+
+    val bot = bot {
+        token = botToken
+        timeout = 30
+        logLevel = LogLevel.Network.Body
+
+        dispatch {
+            command("start") {
+                if (!hasAccess(update.message?.chat?.username)) {
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(update.message!!.chat.id),
+                        text = Strings.greetingsBad,
+                        parseMode = MARKDOWN
+                    )
+                    return@command
+                }
+
+                val inlineKeyboardMarkup = InlineKeyboardMarkup.create(
+                    listOf(InlineKeyboardButton.CallbackData(text = Strings.menuBtnText, callbackData = "menu")),
+                    listOf(InlineKeyboardButton.CallbackData(text = Strings.tableBtnText, callbackData = "table"))
+                )
+
+                bot.sendMessage(
+                    chatId = ChatId.fromId(update.message!!.chat.id),
+                    text = Strings.greetingsOk,
+                    parseMode = MARKDOWN,
+                    replyMarkup = inlineKeyboardMarkup
+                )
+
+            }
+
+            callbackQuery("menu") {
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.sendMessage(ChatId.fromId(chatId), text = Strings.menuClicked)
+            }
+
+            callbackQuery("table") {
+                val chatId = callbackQuery.message?.chat?.id ?: return@callbackQuery
+                bot.sendMessage(ChatId.fromId(chatId), text = Strings.tableClicked)
+            }
+
+            photos {
+                if (!hasAccess(message.chat.username)) return@photos
+                bot.sendMessage(ChatId.fromId(message.chat.id), Strings.hintUseFullPhoto)
+            }
+
+            document {
+                if (!hasAccess(message.chat.username)) return@document
+                val document = message.document ?: return@document
+
+                println("Attached document: $document")
+                val extension = document.fileName?.split('.')?.last() ?: return@document
+                when (extension) {
+                    "jpg", "jpeg", "png" -> interactor.downloadFile(bot, document) {
+
+                    }
+                }
+                /*if (photo.fileId == lastProcessedImage) {
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = Strings.photoAlreadyProcessed
+                    )
+                    return@photos
+                }
+                message.chat.type*/
+
+                /*interactor.downloadFile(bot, photo.fileId) { file ->
+                    if (file == null) {
+                        println("Cannot load file")
+                        return@downloadFile
+                    }
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id),
+                        text = "Успешно скачал файл ${file.name}"
+                    )
+                }
+                lastProcessedImage = photo.fileId*/
+            }
+
+            telegramError {
+                println(error.getErrorMessage())
+            }
+        }
+    }
+
+    bot.startPolling()
 }
+
+private fun hasAccess(username: String?) =
+    if (username == null || username.isBlank())
+        false
+    else
+        allowedUsers.contains(username)
