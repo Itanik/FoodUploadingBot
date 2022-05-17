3af442860af46b258bb76cf7f4ee347e367607da
Nikita Ulyantsev <yan.xhrome@gmail.com>
2022-05-17 17:15:55 +0300

    Add additional fields in json files.

--- src/main/kotlin/ftp/data/Food.kt
+++ src/main/kotlin/ftp/data/Food.kt
@@ -5,5 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class Food(
     val name: String,
-    val path: String
+    val path: String,
+    val lastModificationDate: String? = null
 )
--- src/main/kotlin/ftp/data/Menu.kt
+++ src/main/kotlin/ftp/data/Menu.kt
@@ -4,5 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Menu(
-    val path: String
+    val path: String,
+    val name: String? = null,
+    val lastModificationDate: String? = null
 )
